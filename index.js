d64a24d99638fb2cea145c78585799935099b802
Donald Soh <sohdonald@gmail.com>
2023-06-01 16:16:07 -0400

    appended the post

--- index.js
+++ index.js
@@ -1,1 +1,109 @@
 // Add your code here
+
+//fetch(destinationURL, configurationObject);
+// const formData = {
+//     dogName: "Byron",
+//     dogBreeed: "Poodle",
+// };
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+
+//     body: JSON.stringify(formData),
+//     };
+
+//     fetch("http://localhost:3000/dogs", configurationObject)
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (object) {
+//         console.log(object);
+//     })
+//     .catch(function (error) {
+//         alert("Bad things! Ragnarők!")
+//         console.log(error.message);
+//     });
+
+
+function submitData(name,email) {
+    //event.preventDefault(); 
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+               
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(object) {
+      const getId = object.id
+      document.body.append(getId);
+    
+      //console.log(getId);
+    })
+
+    .catch(function(error) {
+        document.body.append(error);
+        
+    })
+    // wants the id number, and appends it to the DOM
+    // takes the post made in submitData, and returns it as an object
+
+    
+}
+
+// preventDefault
+
+function patchData(name,email) {
+    fetch("http://localhost:3000/users/", {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+
+        body: JSON.stringify({
+            userName: name,
+            userEmail: email,
+        }),
+    });
+}
+
+function putData(name,email) {
+    fetch("http://localhost:3000/users", {
+        method: "PUT",
+        headers: { // info about our request
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+
+        body: JSON.stringify({ //.stringify converts data into json
+            userName: name,
+            userEmail: email,
+        }),
+    });
+}
+
+function deleteData(name, email) {
+    fetch("http://localhost:3000/users", { // this is an object
+        method: "DELETE", // type of operation
+     
+    });
+}
+
+deleteData("userName","userEmail");
+//
+
+
